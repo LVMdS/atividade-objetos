01eb3256bcc4c6db05c69d0dd578c63a63eeb061
LVMdS <leo92vinicius@gmail.com>
2024-03-21 21:03:36 -0300

    aj

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-// Array para armazenar os produtos adicionados ao carrinho
+
 let carrinho = [];
 
 // Função para adicionar um produto ao carrinho
 function adicionarProduto(nome, precoUnitario, quantidade) {
-    // Calcular subtotal do produto
+    
     let subtotal = precoUnitario * quantidade;
 
     // Adicionar o produto ao carrinho como um objeto
@@ -14,14 +14,13 @@ function adicionarProduto(nome, precoUnitario, quantidade) {
         subtotal: subtotal
     });
 
-    // Atualizar a exibição do carrinho
+   
     exibirCarrinho();
 }
 
 // Função para remover um produto do carrinho
 function removerProduto(index) {
-    carrinho.splice(index, 1); // Remove o item do array do carrinho na posição index
-    // Atualizar a exibição do carrinho
+    carrinho.splice(index, 1); 
     exibirCarrinho();
 }
 
@@ -30,8 +29,6 @@ function exibirCarrinho() {
     console.log("\nProdutos no Carrinho:");
 
     let totalCompra = 0;
-
-    // Limpar lista de produtos antes de exibir
     document.getElementById("listaProdutos").innerHTML = '';
 
     // Iterar sobre cada produto no carrinho
@@ -56,13 +53,13 @@ function exibirCarrinho() {
         document.getElementById("listaProdutos").innerHTML += produtoHTML;
     });
 
-    // Atualizar total da compra no HTML
+
     document.getElementById("total").innerText = `Total da Compra: R$ ${totalCompra.toFixed(2)}`;
 }
 
 // Event listener para o formulário de adição de produtos
 document.getElementById("produtoForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evita que o formulário seja enviado
+    event.preventDefault(); 
 
     let nome = document.getElementById("nome").value;
     let precoUnitario = parseFloat(document.getElementById("preco").value);

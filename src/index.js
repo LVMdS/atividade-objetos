960c12126b98aa8c1206bb34472d22fb73366faf
LVMdS <leo92vinicius@gmail.com>
2024-03-23 18:27:10 -0300

    $

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-const prompt = require('prompt-sync')(); // Importando a biblioteca para receber entrada do usuário
+const prompt = require('prompt-sync')(); 
 
-// Definindo a função para calcular o subtotal de um item
 function calcularSubtotal(preco, quantidade) {
     return preco * quantidade;
 }
 
-// Solicitando informações do cliente
 console.log("Por favor, forneça suas informações:");
 const nomeCliente = prompt("Nome: ");
 const emailCliente = prompt("Email: ");
 const enderecoCliente = prompt("Endereço: ");
 
-// Inicializando o carrinho como um array vazio
 let carrinho = [];
 
 // Loop para adicionar produtos ao carrinho
@@ -29,7 +26,7 @@ while (true) {
         preco: preco,
         quantidade: quantidade,
         subtotal: subtotal
-    });
+});
 
     const continuar = prompt("Deseja adicionar mais produtos? (s/n): ");
     if (continuar.toLowerCase() !== 's') {
@@ -44,11 +41,9 @@ for (const item of carrinho) {
     console.log(`${item.nome} - Preço: R$${item.preco.toFixed(2)} - Quantidade: ${item.quantidade} - Subtotal: R$${item.subtotal.toFixed(2)}`);
     totalCompra += item.subtotal;
 }
-// Exibindo informações do cliente
+console.log(`\nTotal da compra: R$${totalCompra.toFixed(2)}`);
+
 console.log("\nInformações do Cliente:");
 console.log(`Nome: ${nomeCliente}`);
 console.log(`Email: ${emailCliente}`);
 console.log(`Endereço: ${enderecoCliente}`);
-
-// Exibindo o total da compra
-console.log(`\nTotal da compra: R$${totalCompra.toFixed(2)}`);
